e3b24388d6213d8309def7eb316f3167f8f4824a
Federico Carrara <fcarrara@d.ethz.ch@bs-dw139.ethz.ch>
2023-07-03 18:09:26 +0200

    Updated files for running on cluster.

--- src/statistics_collection/submit_jobs.py
+++ src/statistics_collection/submit_jobs.py
@@ -41,7 +41,7 @@ def create_slurm(
 #SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=16384
 
-python src/run.py --config {config_file}   
+python src/statistics_collection/run.py --config {config_file}   
     """
 
     script_file = f'submit_training_{name}.sh'
@@ -57,23 +57,25 @@ python src/run.py --config {config_file}
 
 
 #Read the config.json file
-with open("src/config.json", "r") as file:
+with open("src/statistics_collection/config.json", "r") as file:
     curr_config = json.load(file)
 
 #Set ranges of parameters to modify in the config
-tissues = ['intestine_villus', 'lung_bronchiole', 'bladder', 'esophagus']
+tissues = ['intestine_villus', 'lung_bronchiole', 'bladder', 'esophagus', 'lung']
 voxel_sizes = [
     [0.325, 0.325, 0.25],
     [0.1625, 0.1625, 0.25],
     [0.21, 0.21, 0.39],
-    [0.1625, 0.1625, 0.25]
+    [0.1625, 0.1625, 0.25],
+    [0.1, 0.1, 0.1]
 ]
 COMMON_ROOT = './curated_labels/'
 input_files = [
     'intestine_sample2_b_curated_segmentation_relabel_seq.tif',
     'lung_new_sample_b_curated_segmentation_central_crop_relabel_seq.tif',
     'bladder_control_curated_segmentation.tif',
-    'esophagus_Z2_curated_crop.tif'
+    'esophagus_Z2_curated_crop.tif',
+    'lung_pseudostratified_from_harold.tif'
 ]
 input_paths = [os.path.join(COMMON_ROOT, input_file) for input_file in input_files]
 
@@ -85,7 +87,7 @@ for tissue, voxel_size, input_path in zip(tissues, voxel_sizes ,input_paths):
     curr_config["input_path"] = input_path
     
     #save the new config dictionary
-    save_config_dir = './configs'
+    save_config_dir = './run_euler/configs'
     if not os.path.exists(save_config_dir):
         os.makedirs(save_config_dir)
     with open(os.path.join(save_config_dir, f"config_{tissue}.json"), "w") as f_out:
@@ -95,7 +97,8 @@ for tissue, voxel_size, input_path in zip(tissues, voxel_sizes ,input_paths):
     path_to_job_script = create_slurm(
         config_file=os.path.join(save_config_dir, f"config_{tissue}.json"), 
         name=tissue,
-        max_workers=curr_config["max_workers"]
+        max_workers=curr_config["max_workers"],
+        jobs_dir='./run_euler/jobs'
     )
     
     #execute the created job  
